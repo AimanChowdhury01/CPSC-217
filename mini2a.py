1e3493c8af28c0c2ddebd319bd611756e04b0ec2
AimanChowdhury01 <135834898+AimanChowdhury01@users.noreply.github.com>
2023-06-12 01:33:28 +0600

    Add files via upload

---
+++ mini2a.py
@@ -0,0 +1,11 @@
+#studentInfo
+#name = Aiman Nawar Chowdhury
+#ID = UCID: 30174339
+#Version: 15 Oct, 2022. 
+def start():
+    revealName()
+def revealName():
+    name = input("Type in your full name: ")
+    print(name)
+start()
+    
