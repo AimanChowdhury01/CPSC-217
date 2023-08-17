57360a28a6a51e5d7b07e56ac774eae53460af8f
AimanChowdhury01 <135834898+AimanChowdhury01@users.noreply.github.com>
2023-08-18 03:20:07 +0600

    Add files via upload

---
+++ firstFunction.py
@@ -0,0 +1,11 @@
+#studentInfo
+#name = Aiman Nawar Chowdhury
+#ID = UCID: 30174339
+#Version: 18 Oct, 2022. 
+def revealName():
+    name = input("Type in your full name: ")
+    print(name)
+def start():
+    revealName()    
+start()
+    
